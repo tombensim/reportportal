c02ba0142c171e581643205327486faa2a6f341b
Yotam <yotam@experitest.com>
2018-03-15 12:30:17 +0200

    doing

---
+++ src/main/java/com/epam/rp/tests/logging/XmlLoggingBetterTest.java
@@ -0,0 +1,26 @@
+package com.epam.rp.tests.logging;
+
+/**
+ * IMPROVED version of file logging example
+ * Uses external utilities
+ *
+ * @author Andrei Varabyeu
+ */
+public class XmlLoggingBetterTest {
+
+	public static final String XML_FILE_PATH = "xml/file.xml";
+
+//	@Test
+//	public void logXmlBase64() throws IOException {
+//		/* here we are logging some binary data as BASE64 string */
+//		LoggingUtils.log(Resources.asByteSource(Resources.getResource(XML_FILE_PATH)).read(), "I'm logging content via BASE64");
+//	}
+//
+//	@Test
+//	public void logXmlFile() throws IOException {
+//		/* here we are logging some binary data as file (useful for selenium) */
+//		File file = File.createTempFile("rp-test", "xml");
+//		Resources.asByteSource(Resources.getResource(XML_FILE_PATH)).copyTo(Files.asByteSink(file));
+//		LoggingUtils.log(file, "I'm logging content via BASE64");
+//	}
+}
